8c3c027c23f63c540963a6441d9a08f42ef0dc22
Amanda <157532117+alp409@users.noreply.github.com>
2024-04-01 13:01:33 -0400

    Added directions to New Location Scriptable Object
    Added north, south, east, west to Scriptable object so that locations can be assigned in relation to each other.

--- CodeLab1_scriptableObject_homework/Assets/Scripts/GameManager.cs
+++ CodeLab1_scriptableObject_homework/Assets/Scripts/GameManager.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI titleUI;
     public TextMeshProUGUI descriptionUI;
 
-    public LocationScriptableObject currentLocation; 
+    public LocationScriptableObject currentLocation;
+
+    public Button buttonNorth;
+    public Button buttonSouth;
+    public Button buttonEast;
+    public Button buttonWest;
     
     // Start is called before the first frame update
     void Start()
@@ -17,9 +23,27 @@ public class GameManager : MonoBehaviour
         currentLocation.UpdateCurrentLocation(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveDir(string dirChar)
     {
+        switch (dirChar)
+        {
+            case "N":
+                currentLocation = currentLocation.north;
+                break;
+            case "S":
+                currentLocation = currentLocation.south;
+                break;
+            case "E":
+                currentLocation = currentLocation.east;
+                break;
+            case "W":
+                currentLocation = currentLocation.west;
+                break;
+            default:
+                Debug.Log("Nope");
+                break;
+        }
         
+        currentLocation.UpdateCurrentLocation(this);
     }
 }
--- CodeLab1_scriptableObject_homework/Assets/Scripts/LocationScriptableObject.cs
+++ CodeLab1_scriptableObject_homework/Assets/Scripts/LocationScriptableObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(
     fileName = "New Location", 
@@ -12,8 +13,15 @@ public class LocationScriptableObject : ScriptableObject
 {
     public string locationName;
     public string locationDesc;
+
+    // below allows us to assign (or view automatically assigned) locations
+    // to the north, south, east, or west of the current location
+    public LocationScriptableObject north;
+    public LocationScriptableObject south;
+    public LocationScriptableObject east;
+    public LocationScriptableObject west;
     
-    
+    // print a string of our locaiton and description in the console
     public void PrintLocation()
     {
         string printStr = "\nLocation Name: " + locationName +
@@ -22,6 +30,7 @@ public class LocationScriptableObject : ScriptableObject
         Debug.Log(printStr);
     }
 
+    // always update the text UI for location and description (in the game manager) to our current locaiton
     public void UpdateCurrentLocation(GameManager gm)
     {
         gm.titleUI.text = locationName;
