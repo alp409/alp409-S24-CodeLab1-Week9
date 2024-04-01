1476a6e7181db8475cb72529bf4f31dd6138ed9f
Amanda <157532117+alp409@users.noreply.github.com>
2024-04-01 12:46:30 -0400

    Added buttons, started scriptable objects and GM
    Added four direction buttons and TMP for location and description.
    Created a Game manager script and empty object.
    Made a scriptable object called New location, and used it to make "Home" and "Yard" to start.  Printing current location to the console, and updating current location in the Game Manager.

---
+++ CodeLab1_scriptableObject_homework/Assets/Scripts/LocationScriptableObject.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "New Location", 
+    menuName = "New Location", 
+    order = 0)
+]
+
+public class LocationScriptableObject : ScriptableObject
+{
+    public string locationName;
+    public string locationDesc;
+    
+    
+    public void PrintLocation()
+    {
+        string printStr = "\nLocation Name: " + locationName +
+                          "\nLocation Description: " + locationDesc;
+    
+        Debug.Log(printStr);
+    }
+
+    public void UpdateCurrentLocation(GameManager gm)
+    {
+        gm.titleUI.text = locationName;
+        gm.descriptionUI.text = locationDesc;
+        
+        
+    }
+}
+
+
